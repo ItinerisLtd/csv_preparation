6faff93a4885297bfd0ee19cb0ad2c49a83e2110
Tang Rufus <tangrufus@gmail.com>
2018-11-12 13:50:44 +0000

    Tidy up HTML

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  root 'screaming_frogs#new'
-
   resources :screaming_frogs, only: [:new, :create]
   resources :safe_redirect_managers, only: [:new, :create]
 end
